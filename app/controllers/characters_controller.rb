3d91eb779a73283d54313fe59a0710e3d3dc8e84
Troy Griggs <troy.griggs@gmail.com>
2014-01-26 22:58:33 -0500

    working out routes between reference and character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,6 +3,7 @@ class CharactersController < ApplicationController
 	def index 
 		@characters = Character.all
 		@episodes = Episode.all
+		@references = Reference.all
 	end
 
 end
--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -4,5 +4,12 @@ class ReferencesController < ApplicationController
 		@characters = Character.all
 		@episodes = Episode.all
 		@alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+		@grouped = {}
+		Reference.all.sort.each do |ref|
+		  letter = ref.subject.slice(0,1).upcase
+		  @grouped[letter] ||= []
+		  @grouped[letter] << ref
+		end
 	end
 end
